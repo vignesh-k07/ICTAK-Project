de872404c8de7f7116e5c29c39b0db1ac5b3f0c7
vignesh k <74788727+vignesh-krishnan07@users.noreply.github.com>
2022-03-16 17:22:07 +0530

    Update Server.js

--- project_backend/server.js
+++ project_backend/server.js
@@ -14,6 +14,7 @@ const CorporateRouter = require('./routes/corporate/CorporateRouter');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
@@ -78,6 +79,6 @@ app.use('/api',LoginRouter);
 //     });
 
 // listen port
-app.listen(process.env.PORT || 8000, () => {
+app.listen(PORT, () => {
     console.log("Server running on port 8000...");
 });
